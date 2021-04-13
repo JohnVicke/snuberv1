ea5c083dc5b140e51aa4ca15ccabc7f22240ee83
Viktor Malmedal <viktormalmedal@gmail.com>
2021-04-13 11:48:17 +0200

    restarts mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,94 +1,23 @@
-import React, { useEffect, useState } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { ApplicationProvider } from '@ui-kitten/components';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-import AppLoading from 'expo-app-loading';
-import {
-  useFonts,
-  Poppins_400Regular as PoppinsRegular,
-  Poppins_700Bold as PoppinsBold,
-  Poppins_600SemiBold as PoppinsSemiBold
-} from '@expo-google-fonts/poppins';
+import useCachedResources from './hooks/useCachedResources';
+import useColorScheme from './hooks/useColorScheme';
+import Navigation from './navigation';
 
-import * as eva from '@eva-design/eva';
-import { default as theme } from './theme.json';
+export default function App() {
+  const isLoadingComplete = useCachedResources();
+  const colorScheme = useColorScheme();
 
-import { RootStackParamList } from './type';
-
-import MainScreen from './components/MainScreen';
-import HomeScreen from './components/HomeScreen';
-import { DatabaseProvider } from './context/DatabaseContext';
-import {
-  AppStateStatus,
-  SafeAreaView,
-  StyleSheet,
-  AppState
-} from 'react-native';
-
-const { Navigator, Screen } = createStackNavigator<RootStackParamList>();
-
-let appState: AppStateStatus;
-
-const RootNavigator = () => (
-  <Navigator headerMode="none" initialRouteName="Home">
-    <Screen name="Home" component={HomeScreen} />
-    <Screen name="Main" component={MainScreen} />
-  </Navigator>
-);
-
-export default () => {
-  appState = AppState.currentState;
-
-  const [fontsLoaded] = useFonts({
-    PoppinsBold,
-    PoppinsRegular,
-    PoppinsSemiBold
-  });
-
-  useEffect(() => {
-    appInForeground();
-    appState = 'active';
-
-    AppState.addEventListener('change', handleAppStateChange);
-
-    return () => {
-      AppState.removeEventListener('change', handleAppStateChange);
-    };
-  }, []);
-
-  const handleAppStateChange = (nextAppState: AppStateStatus) => {
-    if (appState.match(/inactive|background/) && nextAppState === 'active') {
-      appInForeground();
-    }
-    appState = nextAppState;
-  };
-
-  const appInForeground = () => {
-    console.log('app running in foreground');
-  };
-
-  if (!fontsLoaded) {
-    return <AppLoading />;
+  if (!isLoadingComplete) {
+    return null;
+  } else {
+    return (
+      <SafeAreaProvider>
+        <Navigation colorScheme={colorScheme} />
+        <StatusBar />
+      </SafeAreaProvider>
+    );
   }
-
-  return (
-    <>
-      <DatabaseProvider>
-        <ApplicationProvider {...eva} theme={{ ...eva.dark, ...theme }}>
-          <SafeAreaView style={styles.container}>
-            <NavigationContainer>
-              <RootNavigator />
-            </NavigationContainer>
-          </SafeAreaView>
-        </ApplicationProvider>
-      </DatabaseProvider>
-    </>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  }
-});
+}
--- mobile/env.d.ts
+++
@@ -1,4 +0,0 @@
-declare module "@env" {
-  export const MAP_BOX_DEFAULT_TOKEN: string;
-  export const ENV: "dev" | "prod";
-}
